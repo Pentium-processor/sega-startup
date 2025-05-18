014371251331ad5f538d986b82f01a34c53731b2
Sys Acrunis 1o1 <114465675+Pentium-processor@users.noreply.github.com>
2025-05-18 11:15:40 +0100

    Update acrunis.py

--- acrunis.py
+++ acrunis.py
@@ -19,9 +19,18 @@ os.system("cls")
 for n in sega_logo:
     sys.stdout.flush()
     time.sleep(0.01)
-    sys.stdout.write(Fore.BLUE+n)
+    sys.stdout.write(font_colors[0]+n)
 else:
-    time.sleep(0.1)
+    time.sleep(1)
+    while len(font_colors)>1:
+        random_color=random.choice(font_colors[1:])
+        os.system("cls")
+        print(random_color+sega_logo)
+        time.sleep(1)
+        if random_color in font_colors:
+            font_colors.remove(random_color)
+        else:
+            pass
     playsound("sega.mp3")
     print(Back.RESET,Fore.RESET)
     os.system("cls")
